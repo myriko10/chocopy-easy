a5e8430f58fe7dbedd50c5dd7871f526e1df1adf
Hyoufms <75256821+Hyoufms@users.noreply.github.com>
2023-12-04 16:09:55 +0900

    定数を大文字にする

--- src/player.py
+++ src/player.py
@@ -6,14 +6,14 @@ from point import Point
 
 class Player:
     def __init__(self, point):
-        self.image_running1 = IMAGEDICT['run1'] # 走り画像1
-        self.image_running2 = IMAGEDICT['run2'] # 走り画像2
-        self.image = self.image_running1 # 初期画像
+        self.IMAGE_RUNNIG1 = IMAGEDICT['run1'] # 走り画像1
+        self.IMAGE_RUNNIG2 = IMAGEDICT['run2'] # 走り画像2
+        self.image = self.IMAGE_RUNNIG1 # 初期画像
         self.current_image = self.image # 現在の画像
         self.default_left_top_point = Point(*point.get_xy()) # point # プレイヤーが地面に着地しているときの座標
         self.default_right_bottom_point = Point(point.x + self.image.get_width(), point.y + self.image.get_height())
         self.position = Point(*point.get_xy()) # プレイヤーの位置
-        self.y_velocity = 0 # y方向の速度
+        self.Y_VELOCITY = 0 # y方向の速度
         self.on_ground = True # 地面にいるかどうか
         self.GRAVITY = 0.5  # 重力
         self.INITIAL_VELOCITY = -15 # ジャンプの初速
@@ -21,7 +21,7 @@ class Player:
     # ジャンプ処理
     def init_jump(self):
         # 速さを更新
-        self.y_velocity = self.INITIAL_VELOCITY
+        self.Y_VELOCITY = self.INITIAL_VELOCITY
         # 地面にいない状態にする
         self.on_ground = False
         # Spaceキーが押されたj状態を記録
@@ -30,9 +30,9 @@ class Player:
     # ジャンプしている間の画像の更新
     def jump(self):
         # 重力を加える
-        self.y_velocity += self.GRAVITY
+        self.Y_VELOCITY += self.GRAVITY
         # 位置を更新
-        self.position.y += self.y_velocity
+        self.position.y += self.Y_VELOCITY
 
         # 地面に着地したか判定
         if self.position.y > self.default_left_top_point.y:
@@ -41,23 +41,23 @@ class Player:
             # 地面に着地したときの位置に矯正
             self.position.y = self.default_left_top_point.y
             # 速度を0にしてプレイヤーが動かないようにする
-            self.y_velocity = 0
+            self.Y_VELOCITY = 0
 
     # 画像を描画
     def switch_image(self):
         # 画像の変更
         if self.on_ground:
-            self.current_image = self.image_running1
+            self.current_image = self.IMAGE_RUNNIG1
             # 時間が等間隔で馬の画像を切り替え、走っているように見せる
             if True:
                 time_now = time.time()
                 if int(time_now) % 2 == 0:
-                    self.current_image = self.image_running1
+                    self.current_image = self.IMAGE_RUNNIG1
                 else:
-                    self.current_image = self.image_running2
+                    self.current_image = self.IMAGE_RUNNIG2
         # ジャンプしている時
         else:
-            self.current_image = self.image_running2
+            self.current_image = self.IMAGE_RUNNIG2
     
     # ゲームオーバー時の画像に切り替え
     def game_over(self):
