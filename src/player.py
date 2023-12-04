717446baf870bdf585a971df92c4a9615ef38e0e
Hyoufms <75256821+Hyoufms@users.noreply.github.com>
2023-12-04 17:40:56 +0900

    player.py修正

--- src/player.py
+++ src/player.py
@@ -4,26 +4,22 @@ from point import Point
 
 class Player:
     def __init__(self, point, HEIGHT):
-        self.IMAGE_RUNNIG1 = IMAGEDICT['run1'] # 走り画像1
-        self.IMAGE_RUNNIG2 = IMAGEDICT['run2'] # 走り画像2
-        self.image = self.IMAGE_RUNNIG1 # 初期画像
-        self.image = self.IMAGE_RUNNIG1 # 現在の画像
+        self.image = IMAGEDICT['run1'] # 現在の画像
         self.default_left_top_point = Point(*point.get_xy()) # point # プレイヤーが地面に着地しているときの座標
-        self.default_right_bottom_point = Point(point.x + self.image.get_width(), point.y + self.image.get_height())
+
         self.left_top_point = Point(*point.get_xy()) # プレイヤーの位置
-        self.Y_VELOCITY = 0 # y方向の速度
-        self.right_bottom_point = Point(point.x + self.image.get_width(), point.y + self.image.get_height())
-        self.left_top_point = Point(*point.get_xy()) # プレイヤーの位置
+
         self.Y_VELOCITY = 0 # y方向の速度
         self.on_ground = True # 地面にいるかどうか
         self.GRAVITY = 0.5  # 重力
         self.INITIAL_VELOCITY = -(HEIGHT / 40) # ジャンプの初速
+        self.right_bottom_point = Point(self.left_top_point.x + self.image.get_width(), 
+                                        self.left_top_point.y + self.image.get_height()) # 右下の座標
 
     # ジャンプ処理
     def init_jump(self):
         # 速さを更新
         self.Y_VELOCITY = self.INITIAL_VELOCITY
-
         # 地面にいない状態にする
         self.on_ground = False
         # Spaceキーが押されたj状態を記録
@@ -50,19 +46,21 @@ class Player:
     # 画像を描画
     def switch_image(self, is_game_over):
         # 画像の変更
-        if self.on_ground:
-            time_now = time.time() * 1000
+        # ゲームオーバーのとき
+        if is_game_over:
+            self.image = IMAGEDICT['error']
+        
+        # ゲーム中で着地しているとき
+        elif self.on_ground:
+            self.image = IMAGEDICT['run1']
             # 時間が等間隔で馬の画像を切り替え、走っているように見せる
-            if int(time_now) % 500 < 250: # 0.5秒ごとに画像を切り替える
-            # if int(time_now) % 100 < 50: # 0.1秒ごとに画像を切り替える
-                self.image = self.IMAGE_RUNNIG1
-            else:
-                self.image = self.IMAGE_RUNNIG2
+            if True:
+                time_now = time.time() * 1000
+                if int(time_now) % 500 < 250:
+                    self.image = IMAGEDICT['run1']
+                else:
+                    self.image = IMAGEDICT['run2']
         # ジャンプしている時
         else:
-            self.image = self.IMAGE_RUNNIG2
-    
-        # ゲームオーバー時の画像に切り替え
-        if is_game_over:
-             self.image = IMAGEDICT['error']
-        
+            self.image = IMAGEDICT['run2']
+
