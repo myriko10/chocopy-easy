56af8fd4673054d8e50616d831781a71406f6c1d
inamori <Genta.Inamori@jp.ricoh.com>
2023-12-06 13:34:20 +0900

    ヒットボックスを調整

--- src/check_collision.py
+++ src/check_collision.py
@@ -10,6 +10,18 @@ def check_collision(player_left_top_point, player_right_bottom_point,
     hurdle_left, hurdle_top = hurdle_left_top_point.get_xy()
     hurdle_right, hurdle_bottom = hurdle_right_bottom_point.get_xy()
 
+    # ヒットボックスを小さくするためのマージン    
+
+    MARGIN_FOR_PLAYER = 15 
+
+    # player_left ,player_top に margin を足す    
+    player_left = player_left + MARGIN_FOR_PLAYER 
+    player_top = player_top + MARGIN_FOR_PLAYER 
+
+    # player_right 、player_bottomから margin を引く
+    player_right = player_right - MARGIN_FOR_PLAYER 
+    player_bottom = player_bottom - MARGIN_FOR_PLAYER 
+
     # 矩形の交差判定
     # バックスラッシュを使うか()で囲むことで長い条件文は改行できる
     if player_left < hurdle_right and hurdle_left < player_right and \
