70ad95b90d2b5a865af1f91e4fbf76e9c7f31997
inamori <Genta.Inamori@jp.ricoh.com>
2023-12-06 15:44:29 +0900

    レビュー後、プログラムを修正

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import random
 # IMAGEDICTを引っ張ってくるためにfrom 拡張子ファイル名 import 引っ張ってきたい名前
 from image_dict import IMAGEDICT
 # 衝突検知
-from is_collision import Is_collision
+from is_collision import is_collision
 # 時間を扱う
 import time
 # Pygameの初期化
@@ -33,8 +33,6 @@ pygame.display.set_caption('HORSE') # 画面のタイトルかな？
 
 # ハードルのシーケンス
 hurdles = []
-
-cloud=[]
 
 # ゲームの内容
 def run_game():
@@ -126,7 +124,7 @@ def run_game():
                 # プレイヤーの右端のx座標をハードルが左に超えていたら
                 if h.left_top_point.x <= player.left_top_point.x + player.image.get_width()-10:
                     # 衝突検知：戻り値は衝突していたらTrue、していなかったらFalse
-                    is_game_over = Is_collision(player.left_top_point, player.right_bottom_point,
+                    is_game_over = is_collision(player.left_top_point, player.right_bottom_point,
                                     h.left_top_point, h.right_bottom_point)
                     
         # ハードルを描画
