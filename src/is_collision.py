aabe4332f24203ff5a9e97c8aa8f6e5ab89085c1
inamori <Genta.Inamori@jp.ricoh.com>
2023-12-07 10:11:35 +0900

    tuple型をlist型に修正

--- src/is_collision.py
+++ src/is_collision.py
@@ -1,15 +1,13 @@
 # ヒットボックスを小さくするためのマージン        
-MARGIN_FOR_PLAYER = (15,15) 
+MARGIN_FOR_PLAYER = 15 
 
 # chatGPTの出力結果をうのみにしているので確認する必要がある11/09 by まるやま
 def is_collision(player_left_top_point, player_right_bottom_point, 
     hurdle_left_top_point, hurdle_right_bottom_point):
     
     # プレイヤーの矩形の左上座標と右下座標、marginあり
-    player_left = tuple(p + b for p,b in zip(player_left_top_point.get_xy(), MARGIN_FOR_PLAYER))
-    player_top = tuple(p + b for p,b in zip(player_left_top_point.get_xy(), MARGIN_FOR_PLAYER))
-    player_right = tuple(p - b for p,b in zip(player_right_bottom_point.get_xy(), MARGIN_FOR_PLAYER))
-    player_bottom = tuple(p - b for p,b in zip(player_right_bottom_point.get_xy(), MARGIN_FOR_PLAYER))
+    player_left, player_top = [p + MARGIN_FOR_PLAYER for p in player_left_top_point.get_xy()]
+    player_right, player_bottom = [p - MARGIN_FOR_PLAYER for p in player_right_bottom_point.get_xy()]
     
     # ハードルの矩形の左上座標と右下座標
     hurdle_left, hurdle_top = hurdle_left_top_point.get_xy()
@@ -17,8 +15,8 @@ def is_collision(player_left_top_point, player_right_bottom_point,
 
     # 矩形の交差判定
     # バックスラッシュを使うか()で囲むことで長い条件文は改行できる
-    if player_left[0] < hurdle_right and hurdle_left < player_right[0] and \
-        player_bottom[1] > hurdle_top:
+    if player_left < hurdle_right and hurdle_left < player_right and \
+        player_bottom > hurdle_top:
         return True  # 衝突が発生
     else:
         return False  # 衝突が発生していない
